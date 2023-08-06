726901b6e2daee081578161d5a697708bf198890
nathantipton <ntipton21@gmail.com>
2023-08-05 21:12:53 -0400

    hide admin route for now

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,6 +1,15 @@
+import { dev } from '$app/environment';
 import { getSpeciesByRegion } from '$lib/ebird';
 import { AlgoliaAdminClient } from '$lib/server/algolia-admin';
+import { error } from '@sveltejs/kit';
 import type { Actions } from './$types';
+
+export const load = async () => {
+    // Hide this route in production
+    if (!dev) {
+        throw error(404, 'Not found');
+    }
+}
 
 
 export const actions: Actions = {
