175d729890c8646348d2b5b797937e0d2d7e04f1
nathantipton <ntipton21@gmail.com>
2023-08-09 10:41:32 -0400

    responsive search in the header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
+	import { writable } from 'svelte/store';
 	import SearchBar from './SearchBar.svelte';
-
+	const showSearchBar = writable(false);
 </script>
 
-<header class="flex flex-row justify-between w-full items-center p-4 gap-8">
-	<a href="/" class="w-56">
-		<img src="/logo_full_white.svg" alt="" />
-	</a>
+{#if !$showSearchBar}
+	<header class="flex flex-row justify-between w-full items-center p-4 gap-8">
+		<a href="/" class="w-56">
+			<img src="/logo_full_white.svg" alt="" />
+		</a>
 
-	<SearchBar class="w-full max-w-md" />
-</header>
+		<button
+			class="md:hidden text-lg btn-circle btn-ghost"
+			on:click={() => showSearchBar.update((v) => !v)}
+		>
+			<i class="fa-solid fa-search" />
+		</button>
+		<SearchBar class="w-full max-w-md hidden md:block" />
+	</header>
+{/if}
+
+{#if $showSearchBar}
+	<header class="flex flex-row justify-between w-full items-center p-4 gap-8">
+		<SearchBar class="w-full" />
+		<button class="btn btn-ghost" on:click={() => showSearchBar.update((v) => !v)}>
+			<i class="fa-solid fa-xmark" />
+		</button>
+	</header>
+{/if}
