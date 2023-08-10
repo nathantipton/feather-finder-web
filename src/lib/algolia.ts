52a02673806b11437838d1359b0dcbbdb8606774
nathantipton <ntipton21@gmail.com>
2023-08-10 13:02:28 -0400

    add fake trending search for now

--- src/lib/algolia.ts
+++ src/lib/algolia.ts
@@ -1,5 +1,6 @@
 import { PUBLIC_ALGOLIA_APP_ID, PUBLIC_ALGOLIA_SEARCH_KEY } from '$env/static/public';
 import type { SearchResponse } from '@algolia/client-search';
+import algoliarecommend, { type RecommendClient } from '@algolia/recommend';
 import algoliasearch, { type SearchClient, type SearchIndex } from 'algoliasearch';
 import aa from 'search-insights';
 import type { SpeciesRecord } from './models/ebird';
@@ -8,6 +9,7 @@ export class AlgoliaSearchClient implements App.SearchClient {
 
     #client: SearchClient;
     #index: SearchIndex;
+    #recommend: RecommendClient;
 
     constructor(index: string = 'species') {
         this.#client = algoliasearch(
@@ -22,6 +24,12 @@ export class AlgoliaSearchClient implements App.SearchClient {
             appId: PUBLIC_ALGOLIA_APP_ID,
             apiKey: PUBLIC_ALGOLIA_SEARCH_KEY,
         });
+
+        // Initialize the recommendation client
+        this.#recommend = algoliarecommend(
+            PUBLIC_ALGOLIA_APP_ID,
+            PUBLIC_ALGOLIA_SEARCH_KEY,
+        )
 
     }
 
@@ -76,5 +84,23 @@ export class AlgoliaSearchClient implements App.SearchClient {
             queryID,
             objectIDs: [objectID],
         });
+    }
+
+    static objectClickedInTrending(queryID: string, objectID: string, position: number, index: string = 'species') {
+        aa('clickedObjectIDsAfterSearch', {
+            eventName: 'Object Clicked in Trending Preview',
+            index,
+            queryID,
+            objectIDs: [objectID],
+            positions: [position],
+        });
+    }
+
+    getTrendingSpecies() {
+        return this.#recommend.getTrendingItems<SpeciesRecord>([
+            {
+                indexName: 'species',
+            }
+        ])
     }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,30 @@
 
 	import NotableSpeciesMap from '$lib/components/maps/NotableSpeciesMap.svelte';
 	import { colorScheme } from '$lib/stores/ui.store';
+
+	// Fake trends data until model is ready
+	const fakeTrends = [
+		{
+			comName: 'Northern Cardinal',
+			speciesCode: 'norcar'
+		},
+		{
+			comName: 'American Robin',
+			speciesCode: 'amerob'
+		},
+		{
+			comName: 'Mourning Dove',
+			speciesCode: 'moudov'
+		},
+		{
+			comName: 'Blue Jay',
+			speciesCode: 'blujay'
+		},
+		{
+			comName: 'Bald Eagle',
+			speciesCode: 'baleag'
+		}
+	];
 </script>
 
 <div class="flex flex-col relative w-full">
@@ -14,8 +38,24 @@
 	>
 		<SearchBar />
 	</div>
-	
-	<div class="px-4">{$colorScheme} | menu options, recently searched, browse by name</div>
+
+	<div class="p-4 container mx-auto flex flex-col justify-start items-stretch gap-8">
+		<section class="flex flex-col justify-start items-center gap-8">
+			<h3><i class="fa-solid fa-chart-line mr-2" />Trending Searches</h3>
+			<div class="flex flex-row gap-4 flex-wrap">
+				{#each fakeTrends as trend, index (trend.speciesCode)}
+					<a
+						href={`/species/${trend.speciesCode}`}
+						class="px-4 py-1 rounded-full border border-base-300"
+					>
+						<div class="whitespace-nowrap">
+							{trend.comName} <i class="fa-solid fa-up-right-from-square fa-xs" />
+						</div>
+					</a>
+				{/each}
+			</div>
+		</section>
+	</div>
 </div>
 
 <style>
