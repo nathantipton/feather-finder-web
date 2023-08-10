86a343c21f40cdf18f9f6b9305058747d6a7f195
nathantipton <ntipton21@gmail.com>
2023-08-10 09:23:30 -0400

    spinner feels more natural here

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,12 @@
 	<div>
 		<NotableSpeciesMap colorScheme={$colorScheme} />
 	</div>
-	<div class="md:-mt-8 bg-base-100 border border-base-300 dark:bg-zinc-800 z-40 container mx-auto md:rounded-xl max-w-2xl mb-4 p-4">
+	<div
+		class="md:-mt-8 bg-base-100 border border-base-300 dark:bg-zinc-800 z-40 container mx-auto md:rounded-xl max-w-2xl mb-4 p-4"
+	>
 		<SearchBar />
 	</div>
-	
+
 	<div class="px-4">{$colorScheme} | menu options, recently searched, browse by name</div>
 </div>
 
