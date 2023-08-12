b1623219d78f698155c3cadc2e155727cb6f15af
nathantipton <ntipton21@gmail.com>
2023-08-12 12:13:18 -0400

    revert pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA({
-		strategies: 'injectManifest',
-		srcDir: 'src',
-	})],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
